35edf37735dc3590d8a8786c9e4998e48b7822da
Anshul Verma <105417856+AnshulV05@users.noreply.github.com>
2024-05-03 04:06:49 +0530

    Added script to create Links

---
+++ Hacker/create_links.sh
@@ -0,0 +1,6 @@
+touch link-dep/libfst.so link-dep/libfst.so.26 link-dep/libfstscript.so link-dep/libfstscript.so.26
+
+ln -sf link-dep/libfst.so.26.0.0 link-dep/libfst.so
+ln -sf link-dep/libfst.so.26.0.0 link-dep/libfst.so.26
+ln -sf link-dep/libfstscript.so.26.0.0 link-dep/libfstscript.so
+ln -sf link-dep/libfstscript.so.26.0.0 link-dep/libfstscript.so.26
